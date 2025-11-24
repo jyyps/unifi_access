856d5e636f032bf64234cdc493d5a043685f84ce
jyyps <69923414+jyyps@users.noreply.github.com>
2025-11-23 20:39:16 -0500

    Update bianary_sensor.py

--- custom_components/unifi_access/bianary_sensor.py
+++ custom_components/unifi_access/bianary_sensor.py
@@ -2,31 +2,34 @@ from homeassistant.components.binary_sensor import BinarySensorEntity
 from .const import DOMAIN
 
 async def async_setup_entry(hass, entry, async_add_entities):
-    """Set up binary sensors."""
-    data = hass.data[DOMAIN][entry.entry_id]["api"]
-    devices = await data.get("devices")
-    entities = []
+    """Set up binary sensors from a config entry."""
+    api = hass.data[DOMAIN][entry.entry_id]["api"]
+    devices = await api.get_devices()
+    sensors = []
 
-    for device in devices.get("doors", []):
-        entities.append(UniFiDoorSensor(device, hass))
+    for device in devices:
+        if device.get("type") == "door":
+            sensors.append(UniFiDoorSensor(device))
 
-    async_add_entities(entities)
+    async_add_entities(sensors, True)
 
 class UniFiDoorSensor(BinarySensorEntity):
-    def __init__(self, device, hass):
-        self._device = device
-        self._hass = hass
-        self._attr_name = device["name"]
+    """Representation of a UniFi Access door sensor."""
+
+    def __init__(self, device):
+        self.device = device
+        self._attr_name = device.get("name", "UniFi Door")
         self._attr_is_on = device.get("state") == "open"
-
-        hass.bus.async_listen("unifi_access_event", self._handle_event)
-
-    def _handle_event(self, event):
-        if event.get("device_id") == self._device["id"]:
-            if event["type"] in ["door_opened", "door_closed"]:
-                self._attr_is_on = event["type"] == "door_opened"
-                self.schedule_update_ha_state()
 
     @property
     def is_on(self):
+        """Return True if the door is open."""
         return self._attr_is_on
+
+    @property
+    def device_info(self):
+        return {
+            "identifiers": {(DOMAIN, self.device.get("id"))},
+            "name": self._attr_name,
+            "model": self.device.get("model", "UniFi Door"),
+        }
