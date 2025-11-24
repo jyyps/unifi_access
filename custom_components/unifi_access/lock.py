a92890e567d647ac068fc49e67d0474a3ec816c1
jyyps <69923414+jyyps@users.noreply.github.com>
2025-11-23 20:39:34 -0500

    Update lock.py

--- custom_components/unifi_access/lock.py
+++ custom_components/unifi_access/lock.py
@@ -2,34 +2,41 @@ from homeassistant.components.lock import LockEntity
 from .const import DOMAIN
 
 async def async_setup_entry(hass, entry, async_add_entities):
-    data = hass.data[DOMAIN][entry.entry_id]["api"]
-    devices = await data.get("devices")
-    entities = []
+    """Set up locks from a config entry."""
+    api = hass.data[DOMAIN][entry.entry_id]["api"]
+    devices = await api.get_devices()
+    locks = []
 
-    for lock in devices.get("locks", []):
-        entities.append(UniFiLock(lock, hass))
+    for device in devices:
+        if device.get("type") == "lock":
+            locks.append(UniFiDoorLock(device))
 
-    async_add_entities(entities)
+    async_add_entities(locks, True)
 
-class UniFiLock(LockEntity):
-    def __init__(self, lock, hass):
-        self._lock = lock
-        self._hass = hass
-        self._attr_name = lock["name"]
-        self._attr_is_locked = lock.get("state") == "locked"
+class UniFiDoorLock(LockEntity):
+    """Representation of a UniFi Access lock."""
 
-        hass.bus.async_listen("unifi_access_event", self._handle_event)
+    def __init__(self, device):
+        self.device = device
+        self._attr_name = device.get("name", "UniFi Lock")
+        self._locked = device.get("locked", True)
 
-    def _handle_event(self, event):
-        if event.get("device_id") == self._lock["id"]:
-            if event["type"] in ["lock_engaged", "lock_released"]:
-                self._attr_is_locked = event["type"] == "lock_engaged"
-                self.schedule_update_ha_state()
+    @property
+    def is_locked(self):
+        return self._locked
 
     async def async_lock(self, **kwargs):
-        # implement lock API call here
-        pass
+        # Call API to lock the door
+        self._locked = True
 
     async def async_unlock(self, **kwargs):
-        # implement unlock API call here
-        pass
+        # Call API to unlock the door
+        self._locked = False
+
+    @property
+    def device_info(self):
+        return {
+            "identifiers": {(DOMAIN, self.device.get("id"))},
+            "name": self._attr_name,
+            "model": self.device.get("model", "UniFi Lock"),
+        }
