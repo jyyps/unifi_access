e53b7b761a499e6374957961fb175f47cf72cc65
jyyps <69923414+jyyps@users.noreply.github.com>
2025-11-23 20:45:35 -0500

    Update config_flow.py

--- custom_components/unifi_access/config_flow.py
+++ custom_components/unifi_access/config_flow.py
@@ -1,7 +1,6 @@
 import voluptuous as vol
 from homeassistant import config_entries
-from homeassistant.core import HomeAssistant
-from .const import DOMAIN, CONF_HOST, CONF_TOKEN
+from .const import DOMAIN, CONF_HOST, CONF_TOKEN, CONF_USERNAME, CONF_PASSWORD
 from .api import UniFiAccessAPI
 
 class UniFiAccessConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
@@ -11,27 +10,36 @@ class UniFiAccessConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     CONNECTION_CLASS = config_entries.CONN_CLASS_LOCAL_PUSH
 
     async def async_step_user(self, user_input=None):
-        """Handle the initial step from the user."""
         errors = {}
 
         if user_input is not None:
-            # Validate host and token
             host = user_input[CONF_HOST]
-            token = user_input[CONF_TOKEN]
-            api = UniFiAccessAPI(host, token, self.hass)
+            token = user_input.get(CONF_TOKEN)
+            username = user_input.get(CONF_USERNAME)
+            password = user_input.get(CONF_PASSWORD)
+
+            api = UniFiAccessAPI(host, token, username, password, self.hass)
 
             try:
                 devices = await api.get_devices()
                 if devices is None:
                     errors["base"] = "cannot_connect"
                 else:
-                    return self.async_create_entry(title=f"UniFi Access ({host})", data=user_input)
+                    return self.async_create_entry(
+                        title=f"UniFi Access ({host})", data=user_input
+                    )
             except Exception:
                 errors["base"] = "cannot_connect"
 
         data_schema = vol.Schema({
             vol.Required(CONF_HOST, default=""): str,
-            vol.Required(CONF_TOKEN, default=""): str
+            vol.Optional(CONF_TOKEN): str,
+            vol.Optional(CONF_USERNAME): str,
+            vol.Optional(CONF_PASSWORD): str
         })
 
-        return self.async_show_form(step_id="user", data_schema=data_schema, errors=errors)
+        return self.async_show_form(
+            step_id="user",
+            data_schema=data_schema,
+            errors=errors
+        )
